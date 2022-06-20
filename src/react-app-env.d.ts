a016bece9bda4a1fa897a021d3c6f538010e62fe
David Kapteyn <dave.kapteyn@gmail.com>
2022-06-19 20:34:30 -0600

    Updates

--- src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "react-dom/client" {
-  // typing module default export as `any` will allow you to access its members without compiler warning
-  var createRoot: any;
-  export { createRoot };
-}
